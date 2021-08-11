38b36de544cc8daf8c6318495ad51a8fdba9ff52
Aleksandr Hrytsenko <32422951+grytsalex@users.noreply.github.com>
2021-08-11 17:28:41 +0300

    create layout for file loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { validateSingleInput } from "./utils";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h2>ELECTRON APP WAS STARTED</h2>
-    </div>
+    <Container className="App">
+      <Row>
+        <Col>
+          <Form.Group controlId="formFileLg" className="mb-3 file-input">
+            <Form.Label className="label">Выберите файл файл для загрузки</Form.Label>
+            <Form.Control
+              type="file"
+              size="lg"
+              onChange={() => validateSingleInput(this)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant="primary">Сгенерировать номера</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { validateSingleInput } from "./formatValidation";
